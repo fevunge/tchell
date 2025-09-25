64083e027efb579af6c81ee2399df29915b3e445
fevunge <developervunge@gmail.com>
2025-09-26 00:27:31 +0100

    chore: using write and read atleast printf and scanf

--- src/tchell.c
+++ src/tchell.c
@@ -1,11 +1,14 @@
 #include "tchell.h"
+#include <string.h>
+#include "./get_next_line/get_next_line.h"
 
 void tch_loop(void)
 {
-  char c;
-  while (1)
-  {
-    printf("tchell >");
-    scanf(" %c", &c);
+  char *input;
+
+  while (1) {
+    write(1, "tcsh !.. ", 9);
+    input = get_next_line(0);
+    write(1, input, strlen(input));
   }
 }
